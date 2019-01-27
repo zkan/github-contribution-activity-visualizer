3a281f0a35384fd65a057d0d2c6783fc30360894
zkan <kan@prontomarketing.com>
2019-01-27 20:26:30 +0700

    Render data in template using Jinja2 :bear:

--- program.py
+++ program.py
@@ -1,10 +1,11 @@
-'''
-Assume everyone turn on private contributions and activity overview.
-'''
+import os
 
+from jinja2 import Environment, FileSystemLoader
 from requests_html import HTMLSession
 
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_FILE = 'template_index.html'
 GITHUB_USERNAMES = [
     'zkan',
     'mesodiar',
@@ -20,15 +21,31 @@ GITHUB_USERNAMES = [
 
 session = HTMLSession()
 
+context = {
+    'title': 'GitHub Contribution Acvitiy Visualizer',
+    'user_activities': [],
+}
 for username in GITHUB_USERNAMES:
-    print(f'<h1>{username}</h1>')
     profile_url = f'https://github.com/{username}'
+
+    profile = {
+        'username': username,
+        'url': profile_url,
+    }
+
     r = session.get(profile_url)
     contribution_graph_canvas = r.html.find(
         f'div[data-url="/{username}"]',
         first=True
     )
     contribution_graph = contribution_graph_canvas.find('svg', first=True)
-    print('<div>')
-    print(contribution_graph.html)
-    print('</div>')
+    profile['graph'] = contribution_graph.html
+
+    context['user_activities'].append(profile)
+
+
+j2_env = Environment(
+    loader=FileSystemLoader(THIS_DIR),
+    trim_blocks=True
+)
+print(j2_env.get_template(TEMPLATE_FILE).render(context))
