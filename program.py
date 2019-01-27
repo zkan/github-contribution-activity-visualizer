0955b0c6c9b0c164cef49653a094d58bc89a434b
zkan <kan@prontomarketing.com>
2019-01-27 09:15:49 +0700

    :art: Improve code format :bear:

--- program.py
+++ program.py
@@ -5,11 +5,13 @@ Assume everyone turn on private contributions and activity overview.
 from requests_html import HTMLSession
 
 
-GITHUB_USERNAMES = ['zkan',
-                    'mesodiar',
-                    'yothinix',
-                    'gatukgl',
-                    'WiNloSt',]
+GITHUB_USERNAMES = [
+    'zkan',
+    'mesodiar',
+    'yothinix',
+    'gatukgl',
+    'WiNloSt',
+]
 
 session = HTMLSession()
 
@@ -17,8 +19,10 @@ for username in GITHUB_USERNAMES:
     print(f'<h1>{username}</h1>')
     profile_url = f'https://github.com/{username}'
     r = session.get(profile_url)
-    contribution_graph_canvas = r.html.find(f'div[data-url="/{username}"]',
-                                            first=True)
+    contribution_graph_canvas = r.html.find(
+        f'div[data-url="/{username}"]',
+        first=True
+    )
     contribution_graph = contribution_graph_canvas.find('svg', first=True)
     print('<div>')
     print(contribution_graph.html)
