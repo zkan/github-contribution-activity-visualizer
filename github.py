5bff07563d2727216bfb3c15bf03da9bd2dd4eb0
zkan <kan@prontomarketing.com>
2019-01-28 07:58:01 +0700

    :recycle: Move logic into functions + separate usernames into module :bear:

---
+++ github.py
@@ -0,0 +1,12 @@
+GITHUB_USERNAMES = [
+    'zkan',
+    'mesodiar',
+    'Thoritie',
+    'sandysaille',
+    'yothinix',
+    'parnpresso',
+    'DaisukiDaYo',
+    'saowaluck',
+    'gatukgl',
+    'WiNloSt',
+]
--- program.py
+++ program.py
@@ -3,49 +3,46 @@ import os
 from jinja2 import Environment, FileSystemLoader
 from requests_html import HTMLSession
 
+from github import GITHUB_USERNAMES
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-TEMPLATE_FILE = 'template_index.html'
-GITHUB_USERNAMES = [
-    'zkan',
-    'mesodiar',
-    'Thoritie',
-    'sandysaille',
-    'yothinix',
-    'parnpresso',
-    'DaisukiDaYo',
-    'saowaluck',
-    'gatukgl',
-    'WiNloSt',
-]
 
-session = HTMLSession()
-
-context = {
-    'title': 'GitHub Contribution Acvitiy Visualizer',
-    'user_activities': [],
-}
-for username in GITHUB_USERNAMES:
-    profile_url = f'https://github.com/{username}'
-
-    profile = {
-        'username': username,
-        'url': profile_url,
+def get_contribution_activities():
+    session = HTMLSession()
+    context = {
+        'title': 'GitHub Contribution Acvitiy Visualizer',
+        'user_activities': [],
     }
+    for username in GITHUB_USERNAMES:
+        profile_url = f'https://github.com/{username}'
 
-    r = session.get(profile_url)
-    contribution_graph_canvas = r.html.find(
-        f'div[data-url="/{username}"]',
-        first=True
+        profile = {
+            'username': username,
+            'url': profile_url,
+        }
+
+        r = session.get(profile_url)
+        contribution_graph_canvas = r.html.find(
+            f'div[data-url="/{username}"]',
+            first=True
+        )
+        contribution_graph = contribution_graph_canvas.find('svg', first=True)
+        profile['graph'] = contribution_graph.html
+
+        context['user_activities'].append(profile)
+
+    return context
+
+
+def render(context):
+    THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+    TEMPLATE_FILE = 'template_index.html'
+    j2_env = Environment(
+        loader=FileSystemLoader(THIS_DIR),
+        trim_blocks=True
     )
-    contribution_graph = contribution_graph_canvas.find('svg', first=True)
-    profile['graph'] = contribution_graph.html
-
-    context['user_activities'].append(profile)
+    print(j2_env.get_template(TEMPLATE_FILE).render(context))
 
 
-j2_env = Environment(
-    loader=FileSystemLoader(THIS_DIR),
-    trim_blocks=True
-)
-print(j2_env.get_template(TEMPLATE_FILE).render(context))
+if __name__ == '__main__':
+    context = get_contribution_activities()
+    render(context)
